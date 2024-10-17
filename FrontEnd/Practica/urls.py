09c105bcd2d8281ffb3294dad79e0b038faab3c9
Chaval044270 <3033373110108@ingenieria.usac.edu.gt>
2024-10-17 03:59:24 -0600

    tercer commit

--- FrontEnd/Practica/urls.py
+++ FrontEnd/Practica/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cargar/', views.cargar, name='cargar'),
 ]
--- FrontEnd/Practica/views.py
+++ FrontEnd/Practica/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 import requests
+from .forms import FileForm
+
+endpoint = 'http://localhost:5000'
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def cargar(request):
+    return render(request, 'cargar.html')
+
+context = {
+    'contenido_archivo': None,
+    'binario_xml': None,
+    'mensaje_error': None,
+    'mensaje_exito': None,
+    'salidad_procesada': None,
+}
